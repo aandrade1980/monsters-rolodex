b32ea20156cc179c5a8b0fe90b090def9502ae17
Alvaro <varito1@gmail.com>
2020-03-25 12:33:45 -0700

    Updates

---
+++ precache-manifest.ed9f17c0d8d56a9b29218e9c51dd5747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bbce618bb34c3f8f720c7892beb9438",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "468bee2c018661b8716d",
+    "url": "/monsters-rolodex/static/css/main.793501c6.chunk.css"
+  },
+  {
+    "revision": "b7025d703d6faac98ebe",
+    "url": "/monsters-rolodex/static/js/2.a0f84a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a0f84a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "468bee2c018661b8716d",
+    "url": "/monsters-rolodex/static/js/main.b33187ff.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
